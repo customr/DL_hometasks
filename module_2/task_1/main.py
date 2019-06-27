36fba49f3e6ac322c1eaa8aa378c8dc5f10cec65
customr <shipicin_max@mail.ru>
2019-06-27 12:55:42 +0400

    Solution fix

--- module_2/task_1/main.py
+++ module_2/task_1/main.py
@@ -1,12 +1,13 @@
 import re
 
 class Tag:
-	def __init__(self, name, content=''):
+	def __init__(self, name, content='', **data):
 		self.name = name
 		self.content = content
+		self.data = data
 
 	def __str__(self):
-		return f'<{self.name}>{self.content}</{self.name}>'
+		return f'<{self.name}{" "*(len(self.data)>0) + " ".join([f"{item}={val}" for item, val in self.data.items()])}>{self.content}</{self.name}>'
 
 	def __repr__(self):
 		return f'{self.name}_{self.content}'
@@ -17,11 +18,10 @@ class Image(Tag):
 	NAME = 'img'
 	NAME_VARIABLES = ('src', 'img', 'image', 'picture')
 
-	def __init__(self, name=None, content=''):
-		if name is None:
-			name = self.NAME
+	def __init__(self, content='', **data):
+		self.data = data
 
-		super().__init__(name, content)
+		super().__init__(self.NAME, content, **data)
 
 	def __str__(self):
 		return super().__str__()
@@ -35,11 +35,10 @@ class Input(Tag):
 	NAME = 'input'
 	NAME_VARIABLES = ('inp', 'input')
 
-	def __init__(self, name=None, content=''):
-		if name is None:
-			name = self.NAME
+	def __init__(self, content='', **data):
+		self.data = data
 
-		super().__init__(name, content)
+		super().__init__(self.NAME, content, **data)
 
 	def __str__(self):
 		return super().__str__()
@@ -53,11 +52,10 @@ class Text(Tag):
 	NAME = 'p'
 	NAME_VARIABLES = ('text', 'txt', 'p')
 
-	def __init__(self, name=None, content=''):
-		if name is None:
-			name = self.NAME
+	def __init__(self, content='', **data):
+		self.data = data
 
-		super().__init__(name, content)
+		super().__init__(self.NAME, content, **data)
 
 	def __str__(self):
 		return super().__str__()
@@ -70,15 +68,13 @@ class Link(Tag):
 	NAME = 'a'
 	NAME_VARIABLES = ('href', 'a')
 
-	def __init__(self, name=None, content='', link=None):
-		self.link = link
-		if name is None:
-			name = self.NAME
+	def __init__(self, content='', **data):
+		self.data = data
 
-		super().__init__(name, content)
+		super().__init__(self.NAME, content, **data)
 
 	def __str__(self):
-		return f'<a href="{self.link}">{self.content}</a>'
+		return super().__str__()
 
 	def __repr__(self):
 		return super().__repr__()
@@ -88,7 +84,7 @@ class TagFactory:
 	def __init__(self):
 		self.tags = []
 
-	def create_tag(self, name, content=''):
+	def create_tag(self, name, content='', **data):
 		assert isinstance(name, str), 'Tag name must be a string-like'
 
 		name = name.lower()
@@ -105,7 +101,7 @@ class TagFactory:
 
 		for tag in TAGS:
 			if rname in tag.NAME_VARIABLES:
-				self.tags.append(tag(name, content))
+				self.tags.append(tag(content, **data))
 				return self.tags[-1]
 
 		raise ValueError(f'Unknown name tag - "{name}"')
@@ -116,8 +112,9 @@ class TagFactory:
 TAGS = [Image, Input, Text, Link]
 
 if __name__ == '__main__':
-	factory = TagFactory()
-	elements = ('image', 'inp', 'a', 'a1', 'a2', 'a12', 'href', 'text', '')
-	for el in elements:
-		print(factory.create_tag(el))
+	# factory = TagFactory()
+	# elements = ('image', 'inp', 'a', 'a1', 'a2', 'a12', 'href', 'text', '')
+	# for el in elements:
+	# 	print(factory.create_tag(el))
 
+	print(factory.create_tag('a', href='adfgadfgd', somedata=124))
