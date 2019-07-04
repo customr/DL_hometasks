ad6c67f91f166024ad7655d606ec8fb865d1a246
customr <shipicin_max@mail.ru>
2019-07-04 14:35:38 +0400

    fix solution

--- module_2/task_3/subtask_1/main.py
+++ module_2/task_3/subtask_1/main.py
@@ -4,27 +4,28 @@ from time import sleep
 
 
 def write(n):
-	global flag
+	global flag, count
 
-	while random.random()<0.8 and flag: #шанс 20% что писатели сочтут книгу дописанной
+	while (random.random()<0.8 and flag) or count==0: #шанс 20% что писатели сочтут книгу дописанной
 		wrt.acquire()
+		count += 1
 		write_time = random.randint(1, 5)
-		sleep(write_time) 
+		sleep(write_time)
 		print(f'\nThe writer #{n} wrote a new part in {write_time} seconds!\n')
 		wrt.release()
 		sleep(0.5)
 		lock.set()
 
 	flag = False
-	print('WRITING STOPED\n')
+	print(f'WRITING STOPED, TOTAL PARTS: {count}\n')
 
 def read(n):
 	global flag
 
 	while flag:
 		lock.wait()
-		sleep(0.05 * n)
 		print(f'The reader #{n} read a new part!')
+		sleep(0.1)
 		lock.clear()
 
 readers = random.randint(3, 10)
@@ -35,10 +36,10 @@ print('TOTAL READERS NUMBER: ', readers)
 lock = threading.Event()
 wrt = threading.Lock()
 flag = True
+count = 0
 
 for i in range(readers):
-	t = threading.Thread(target=read, args=(i,))
-	t.start()
+	threading.Thread(target=read, args=(i,)).start()
 
 for i in range(writers):
 	t = threading.Thread(target=write, args=(i,))
