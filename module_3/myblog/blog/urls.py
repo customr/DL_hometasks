560b3ad6813422c61260d0ddd75276db6fa243f0
customr <shipicin_max@mail.ru>
2019-08-04 14:37:28 +0400

    new commit

--- module_3/myblog/blog/urls.py
+++ module_3/myblog/blog/urls.py
@@ -6,11 +6,11 @@ from blog import views
 app_name = 'blog'
 urlpatterns = [
 	path('', views.index, name='index-page'),
-	path('blog/<int:blog_id>/', views.blog, name='blog'),
-	path('blogs/', views.get_blogs, name='blogs'),
+	path('blog/<str:blog_name>/', views.blog, name='blog'),
 	path('post/<int:post_id>/', views.post, name='post'),
 	path('posts/', views.get_posts, name='posts'),
+	path('posts/topic/<str:topic>', views.get_posts, name='posts_topic'),
 	path('author/<int:author_id>/', views.author, name='author'),
 	path('authors/', views.get_authors, name='authors'),
-	path('archive/', views.archive, name='archive'),
+	path('help/', views.help, name='help'),
 ]
--- module_3/myblog/blog/views.py
+++ module_3/myblog/blog/views.py
@@ -1,5 +1,7 @@
+import datetime
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.utils import timezone
 
 from blog.models import Author, Blog, Post
 
@@ -9,24 +11,29 @@ def index(request):
 	context = {'blogs': blogs}
 	return render(request, 'blog/index.html', context)
 
-def blog(request, blog_id):
-	blog = get_object_or_404(Blog, pk=blog_id)
-	context = {'blog': blog}
+def blog(request, blog_name):
+	blog = get_object_or_404(Blog, name__iexact=blog_name)
+	posts = Post.objects.all().filter(blog=blog)
+	context = {
+		'blog': blog,
+		'posts': posts
+		}
 	return render(request, 'blog/blog_detail.html', context)
-
-def get_blogs(request):
-	blogs = Blog.objects.all()
-	context = {'blogs': blogs}
-	return render(request, 'blog/blog.html', context)
 
 def post(request, post_id):
 	post = get_object_or_404(Post, pk=post_id)
 	context = {'post': post}
 	return render(request, 'blog/post_detail.html', context)
 
-def get_posts(request):
+
+def get_posts(request, topic=None):
 	posts = Post.objects.all()
-	context = {'posts': posts}
+	if topic is not None:
+		posts = posts.filter(topic__iexact=topic)
+	context = {
+		'posts': posts,
+		'topic': topic
+		}
 	return render(request, 'blog/post.html', context)
 
 def author(request, author_id):
@@ -46,7 +53,5 @@ def get_authors(request):
 	context = {'authors': authors}
 	return render(request, 'blog/author.html', context)
 
-def archive(request):
-	posts_qs = Post.objects.all().order_by('-date_published')
-	context = {'posts_qs': posts_qs}
-	return render(request, 'blog/archive.html', context)
+def help(request):
+	return render(request, 'blog/help.html', {})
